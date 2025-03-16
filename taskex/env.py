aca8b466247ea51c7653706be1b2425548e25692
Ada Lundhe <sean.corbett@umontana.edu>
2025-03-16 12:39:08 -0500

    AL: remove psutil

--- taskex/env.py
+++ taskex/env.py
@@ -1,15 +1,16 @@
-from typing import Callable, Dict, Union, Literal
+import os
+from typing import Callable, Dict, Literal, Union
 
-import psutil
 from pydantic import BaseModel, StrictInt, StrictStr
+
 PrimaryType = Union[str, int, float, bytes, bool]
 
 
 class Env(BaseModel):
-    MERCURY_SYNC_EXECUTOR_TYPE: Literal['thread', 'process'] = 'process'
+    MERCURY_SYNC_EXECUTOR_TYPE: Literal["thread", "process"] = "process"
     MERCURY_SYNC_LOG_LEVEL: StrictStr = "info"
-    MERCURY_SYNC_CLEANUP_INTERVAL: StrictStr = '1s'
-    MERCURY_SYNC_TASK_RUNNER_MAX_THREADS: StrictInt = psutil.cpu_count(logical=False)
+    MERCURY_SYNC_CLEANUP_INTERVAL: StrictStr = "1s"
+    MERCURY_SYNC_TASK_RUNNER_MAX_THREADS: StrictInt = os.cpu_count()
     MERCURY_SYNC_MAX_RUNNING_WORKFLOWS: StrictInt = 1
     MERCURY_SYNC_MAX_PENDING_WORKFLOWS: StrictInt = 100
     MERCURY_SYNC_CONTEXT_POLL_RATE: StrictStr = "0.1s"
